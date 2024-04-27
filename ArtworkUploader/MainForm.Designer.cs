0c1cf8c6a43bb7d5e822c09fc7390405dd16b0db
Isaac Schemm <isaacschemm@gmail.com>
2024-04-27 14:18:11 -0500

    Update icon again

--- ArtworkUploader/MainForm.Designer.cs
+++ ArtworkUploader/MainForm.Designer.cs
@@ -35,7 +35,15 @@
 			weasylToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			btnOpen = new System.Windows.Forms.Button();
+			tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			button1 = new System.Windows.Forms.Button();
+			tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			btnAbout = new System.Windows.Forms.Button();
 			menuStrip1.SuspendLayout();
+			tableLayoutPanel1.SuspendLayout();
+			tableLayoutPanel3.SuspendLayout();
 			SuspendLayout();
 			// 
 			// menuStrip1
@@ -45,7 +53,7 @@
 			menuStrip1.Location = new System.Drawing.Point(0, 0);
 			menuStrip1.Name = "menuStrip1";
 			menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
-			menuStrip1.Size = new System.Drawing.Size(390, 24);
+			menuStrip1.Size = new System.Drawing.Size(284, 24);
 			menuStrip1.TabIndex = 0;
 			menuStrip1.Text = "menuStrip1";
 			// 
@@ -59,14 +67,14 @@
 			// openToolStripMenuItem
 			// 
 			openToolStripMenuItem.Name = "openToolStripMenuItem";
-			openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
 			openToolStripMenuItem.Text = "&Open...";
 			openToolStripMenuItem.Click += openToolStripMenuItem_Click;
 			// 
 			// exitToolStripMenuItem
 			// 
 			exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
 			exitToolStripMenuItem.Text = "E&xit";
 			exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
 			// 
@@ -119,19 +127,98 @@
 			aboutToolStripMenuItem.Text = "&About";
 			aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
 			// 
+			// btnOpen
+			// 
+			btnOpen.Dock = System.Windows.Forms.DockStyle.Fill;
+			btnOpen.Location = new System.Drawing.Point(53, 3);
+			btnOpen.Name = "btnOpen";
+			btnOpen.Size = new System.Drawing.Size(154, 24);
+			btnOpen.TabIndex = 1;
+			btnOpen.Text = "Open file...";
+			btnOpen.UseVisualStyleBackColor = true;
+			// 
+			// tableLayoutPanel1
+			// 
+			tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+			tableLayoutPanel1.ColumnCount = 3;
+			tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+			tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			tableLayoutPanel1.Controls.Add(btnOpen, 1, 0);
+			tableLayoutPanel1.Location = new System.Drawing.Point(12, 27);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 1;
+			tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			tableLayoutPanel1.Size = new System.Drawing.Size(260, 30);
+			tableLayoutPanel1.TabIndex = 3;
+			// 
+			// tableLayoutPanel2
+			// 
+			tableLayoutPanel2.ColumnCount = 3;
+			tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+			tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			tableLayoutPanel2.Name = "tableLayoutPanel2";
+			tableLayoutPanel2.RowCount = 1;
+			tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			tableLayoutPanel2.Size = new System.Drawing.Size(200, 100);
+			tableLayoutPanel2.TabIndex = 0;
+			// 
+			// button1
+			// 
+			button1.Dock = System.Windows.Forms.DockStyle.Fill;
+			button1.Location = new System.Drawing.Point(23, 3);
+			button1.Name = "button1";
+			button1.Size = new System.Drawing.Size(154, 94);
+			button1.TabIndex = 1;
+			button1.Text = "Open file...";
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// tableLayoutPanel3
+			// 
+			tableLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+			tableLayoutPanel3.ColumnCount = 3;
+			tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+			tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			tableLayoutPanel3.Controls.Add(btnAbout, 1, 0);
+			tableLayoutPanel3.Location = new System.Drawing.Point(12, 63);
+			tableLayoutPanel3.Name = "tableLayoutPanel3";
+			tableLayoutPanel3.RowCount = 1;
+			tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			tableLayoutPanel3.Size = new System.Drawing.Size(260, 30);
+			tableLayoutPanel3.TabIndex = 4;
+			// 
+			// btnAbout
+			// 
+			btnAbout.Dock = System.Windows.Forms.DockStyle.Fill;
+			btnAbout.Location = new System.Drawing.Point(73, 3);
+			btnAbout.Name = "btnAbout";
+			btnAbout.Size = new System.Drawing.Size(114, 24);
+			btnAbout.TabIndex = 1;
+			btnAbout.Text = "About";
+			btnAbout.UseVisualStyleBackColor = true;
+			// 
 			// MainForm
 			// 
 			AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			ClientSize = new System.Drawing.Size(390, 417);
+			ClientSize = new System.Drawing.Size(284, 161);
+			Controls.Add(tableLayoutPanel3);
+			Controls.Add(tableLayoutPanel1);
 			Controls.Add(menuStrip1);
 			Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
 			MainMenuStrip = menuStrip1;
 			Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
 			Name = "MainForm";
-			Text = "CrosspostSharp 5";
+			Text = "Artwork Uploader 6.0";
 			menuStrip1.ResumeLayout(false);
 			menuStrip1.PerformLayout();
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel3.ResumeLayout(false);
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -148,6 +235,12 @@
 		private System.Windows.Forms.ToolStripMenuItem weasylToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.Button btnAbout;
 	}
 }
 
