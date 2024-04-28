4bf94fca2fafb85214237f96a5b52bbcef5a6617
Isaac Schemm <isaacschemm@gmail.com>
2024-04-28 14:09:09 -0500

    Untrack OAuthConsumer.cs

--- ArtworkUploader/OAuthConsumer.cs
+++
@@ -1,8 +0,0 @@
-namespace ArtworkUploader {
-	public static class OAuthConsumer {
-		public static class DeviantArt {
-			public readonly static int CLIENT_ID = 6605;
-			public readonly static string CLIENT_SECRET = "";
-		}
-	}
-}
